4373b95e6e988bffb8b92ba8329d8d6f2df3c9fb
Anthony G <givansanthony702@gmail.com>
2023-09-10 16:52:13 -0400

    simple updates

--- model/u_net.py
+++ model/u_net.py
@@ -72,6 +72,52 @@ class DecoderBlock(nn.Module):
         return self.conv(x)
 
 
+class Encoder(nn.Module):
+    """
+    Encoder for UNet Architecture
+    """
+
+    def __init__(self, in_channels: int = 3) -> None:
+        super().__init__()
+
+        self.e1 = EncoderBlock(in_channels=in_channels, out_channels=64)
+        self.e2 = EncoderBlock(in_channels=64, out_channels=128)
+        self.e3 = EncoderBlock(in_channels=128, out_channels=256)
+        self.e4 = EncoderBlock(in_channels=256, out_channels=512)
+
+    def forward(self, x: torch.Tensor) -> tuple[list[torch.Tensor], torch.Tensor]:
+        skip1, u1 = self.e1(x)
+        skip2, u2 = self.e2(u1)
+        skip3, u3 = self.e3(u2)
+        skip4, u4 = self.e4(u3)
+        return [skip1, skip2, skip3, skip4], u4
+
+
+class Decoder(nn.Module):
+    """ "
+    Decoder for the UNet Architecture
+    """
+
+    def __init__(self, in_channels: int) -> None:
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.d1 = DecoderBlock(in_channels=in_channels, out_channels=in_channels // 2)
+        self.in_channels //= 2
+        self.d2 = DecoderBlock(in_channels=self.in_channels, out_channels=self.in_channels // 2)
+        self.in_channels //= 2
+        self.d3 = DecoderBlock(in_channels=self.in_channels, out_channels=self.in_channels // 2)
+        self.in_channels //= 2
+        self.d4 = DecoderBlock(in_channels=self.in_channels, out_channels=self.in_channels // 2)
+
+    def forward(self, x: torch.Tensor, skip_connections: list[torch.Tensor]) -> torch.Tensor:
+        d1 = self.d1(x, skip_connections[-1])
+        d2 = self.d2(d1, skip_connections[2])
+        d3 = self.d3(d2, skip_connections[1])
+        d4 = self.d4(d3, skip_connections[0])
+        return d4
+
+
 class UNet(nn.Module):
     """
     UNet Architecture
@@ -81,36 +127,40 @@ class UNet(nn.Module):
         super().__init__()
 
         """ ENCODER """
-        self.e1 = EncoderBlock(in_channels=in_channels, out_channels=64)
-        self.e2 = EncoderBlock(in_channels=64, out_channels=128)
-        self.e3 = EncoderBlock(in_channels=128, out_channels=256)
-        self.e4 = EncoderBlock(in_channels=256, out_channels=512)
+        # self.e1 = EncoderBlock(in_channels=in_channels, out_channels=64)
+        # self.e2 = EncoderBlock(in_channels=64, out_channels=128)
+        # self.e3 = EncoderBlock(in_channels=128, out_channels=256)
+        # self.e4 = EncoderBlock(in_channels=256, out_channels=512)
+        self.encoder = Encoder(in_channels=in_channels)
 
         """ BOTTLENECK """
         self.b = ConvBlock(in_channels=512, out_channels=1024)
 
         """ DECODER """
-        self.d1 = DecoderBlock(in_channels=1024, out_channels=512)
-        self.d2 = DecoderBlock(in_channels=512, out_channels=256)
-        self.d3 = DecoderBlock(in_channels=256, out_channels=128)
-        self.d4 = DecoderBlock(in_channels=128, out_channels=64)
+        # self.d1 = DecoderBlock(in_channels=1024, out_channels=512)
+        # self.d2 = DecoderBlock(in_channels=512, out_channels=256)
+        # self.d3 = DecoderBlock(in_channels=256, out_channels=128)
+        # self.d4 = DecoderBlock(in_channels=128, out_channels=64)
+        self.decoder = Decoder(in_channels=1024)
 
         """ Classifier """
         self.output = nn.Conv2d(64, num_classes, kernel_size=1, padding=0)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        skip1, u1 = self.e1(x)
-        skip2, u2 = self.e2(u1)
-        skip3, u3 = self.e3(u2)
-        skip4, u4 = self.e4(u3)
+        # skip1, u1 = self.e1(x)
+        # skip2, u2 = self.e2(u1)
+        # skip3, u3 = self.e3(u2)
+        # skip4, u4 = self.e4(u3)
+        skip_connections, encoded = self.encoder(x)
 
-        b = self.b(u4)
+        b = self.b(encoded)
 
-        d1 = self.d1(b, skip4)
-        d2 = self.d2(d1, skip3)
-        d3 = self.d3(d2, skip2)
-        d4 = self.d4(d3, skip1)
-        return self.output(d4)
+        # d1 = self.d1(b, skip4)
+        # d2 = self.d2(d1, skip3)
+        # d3 = self.d3(d2, skip2)
+        # d4 = self.d4(d3, skip1)
+        decoded = self.decoder(b, skip_connections)
+        return self.output(decoded)
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
